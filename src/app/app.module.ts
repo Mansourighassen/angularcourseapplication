ee9be7ae570aa0f280cc37299af0adc24f1e83f0
Mansourighassen <Mansouri.ghassen@esprit.tn>
2020-07-18 02:01:35 -1100

    added home,about,contact and used routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,24 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { DishdetailsComponent } from "./dishdetails/dishdetails.component";
 import { DishService } from "./services/dish.service";
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from "./about/about.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailsComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -26,6 +40,7 @@ import { FooterComponent } from './footer/footer.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    AppRoutingModule,
   ],
   providers: [DishService],
   bootstrap: [AppComponent],
